5e1c37b63f3c3bba91be0f8ec48d2bdccfbbcd82
Tomas Gonzalez <tomasg88@gmail.com>
2022-11-20 19:46:45 -0300

    check active route

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,13 +1,42 @@
+import clsx from 'clsx';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
+const PAGES = [
+  {
+    href: '/about',
+    title: 'ABOUT',
+  },
+  {
+    href: '/services',
+    title: 'SERVICIOS',
+  },
+  {
+    href: '/downloads',
+    title: 'RECURSOS GRATUITOS',
+  },
+  {
+    href: '/contact',
+    title: 'CONTACTO',
+  },
+];
+
 export default function Navigation() {
+  const router = useRouter();
   return (
-    <nav>
-      <Link href="/about">ABOUT</Link>
-      <Link href="/services">SERVICIOS</Link>
-      <Link href="/downloads">RECURSOS GRATUITO</Link>
-      <Link href="/contact">CONTACTO</Link>
+    <nav className="space-x-8">
+      {PAGES.map(({ href, title }) => (
+        <Link
+          className={clsx('tracking-wide text-purple-400 pb-2 font-medium', {
+            'border-b-2 border-purple-400': router.pathname === href,
+          })}
+          href={href}
+          key={href}
+        >
+          {title}
+        </Link>
+      ))}
     </nav>
   );
 }
