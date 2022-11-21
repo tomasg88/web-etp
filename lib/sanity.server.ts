e8b4b9f1c9f42461e1f6ab0cb7605178ec2515c7
Tomas Gonzalez Soldavini <tomasg88@gmail.com>
2022-11-21 14:25:20 -0300

    Feat/queries groq (#5)
    * disable layout for cms views
    
    * update schema for downloads
    
    * groq queries setup
    
    * use groq query to get downloads
    
    * fix ts error
    
    * move hook
    
    * add fields in query
    
    * useCdn in sanity client
    
    * sanity configs altogether

--- lib/sanity.server.ts
+++ lib/sanity.server.ts
@@ -1,4 +1,4 @@
-import { ClientConfig, createClient } from "next-sanity";
-import { config } from "./sanity.config";
+import { createClient } from "next-sanity";
+import { clientConfig } from "./sanity.config";
 
-export const getClient = () => createClient(config as ClientConfig);
+export const getClient = () => createClient(clientConfig);
