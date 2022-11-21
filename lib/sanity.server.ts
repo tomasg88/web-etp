bbdcbc48139b08516b334174c64cf59bc48b0e52
Tomas Gonzalez <tomasg88@gmail.com>
2022-11-21 14:13:36 -0300

    useCdn in sanity client

--- lib/sanity.server.ts
+++ lib/sanity.server.ts
@@ -1,4 +1,8 @@
-import { ClientConfig, createClient } from "next-sanity";
-import { config } from "./sanity.config";
+import { createClient } from "next-sanity";
 
-export const getClient = () => createClient(config as ClientConfig);
+export const getClient = () => createClient({
+  apiVersion: '2021-10-21',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  useCdn: true
+});
