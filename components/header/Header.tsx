c498692d734630f11a13d6d0eac9cf0f7f269730
Tomas Gonzalez <tomasg88@gmail.com>
2022-11-21 17:13:41 -0300

    responsive

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,7 +3,7 @@ import Navigation from '../navigation/Navigation';
 
 const Header = () => {
   return (
-    <header className="absolute flex justify-between px-20 py-12 max-w-7xl w-full mx-auto right-0 left-0">
+    <header className="absolute flex justify-between px-20 py-12 2xl:max-w-7xl w-full mx-auto right-0 left-0">
       <div id="logo">LOGO</div>
       <Navigation />
     </header>
