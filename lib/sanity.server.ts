41474bea686d0a9074f34c43a149a27a372fe7cf
Tomas Gonzalez <tomasg88@gmail.com>
2022-11-21 14:18:47 -0300

    sanity configs altogether

--- lib/sanity.server.ts
+++ lib/sanity.server.ts
@@ -1,8 +1,4 @@
 import { createClient } from "next-sanity";
+import { clientConfig } from "./sanity.config";
 
-export const getClient = () => createClient({
-  apiVersion: '2021-10-21',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  useCdn: true
-});
+export const getClient = () => createClient(clientConfig);
