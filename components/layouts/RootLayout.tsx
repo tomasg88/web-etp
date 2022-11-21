1544a75cc755b252f7f308f3ac66cd5a83e89692
Tomas Gonzalez <tomasg88@gmail.com>
2022-11-20 21:14:23 -0300

    fix ts error

--- components/layouts/RootLayout.tsx
+++ components/layouts/RootLayout.tsx
@@ -6,11 +6,11 @@ import Header from '../header/Header';
 
 const ADMIN_ROUTE = '/admin';
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }): JSX.Element {
   const { pathname } = useRouter();
   const isCMSView = pathname.includes(ADMIN_ROUTE);
 
-  if (isCMSView) return children;
+  if (isCMSView) return <>{children}</>;
 
   return (
     <>
