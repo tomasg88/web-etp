543fb9a227676a177af86fec924288b8fca1fbe3
Tomas Gonzalez <tomasg88@gmail.com>
2022-11-21 13:39:07 -0300

    add fields in query

--- lib/queries.ts
+++ lib/queries.ts
@@ -3,6 +3,7 @@ import { getClient } from "./sanity.server";
 
 export const getDownloadQuery = async () => {
   return await getClient().fetch(
-    groq`*[_type == "free-resources"] {_id, name, link, description}`
+    groq`*[_type == "free-resources" && active == true] 
+      {_id, name, link, description, cover, ctaButton}`
   );
 } 
