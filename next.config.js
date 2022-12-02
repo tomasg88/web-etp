39beae85113ca71179ae900f82dac0547fb881e0
Tomas Gonzalez <tomasg88@gmail.com>
2022-12-02 01:30:49 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['cdn.sanity.io'],
+    loader: 'custom',
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
