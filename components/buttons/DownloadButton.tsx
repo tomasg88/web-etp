e775ed138ac0191c07dbf1221bd99b782629ac99
Tomas Gonzalez <tomasg88@gmail.com>
2022-12-02 19:46:16 -0300

    use font variables and tailwind classes

--- components/buttons/DownloadButton.tsx
+++ components/buttons/DownloadButton.tsx
@@ -9,7 +9,7 @@ interface DownloadButtonProps {
 export const DownloadButton = ({ text }: DownloadButtonProps) => {
   return (
     <button
-      className={`${proximaNova.className} flex items-center bg-gold-400 rounded-lg text-white py-4 px-6`}
+      className={`${proximaNova.variable} font-proxima-nova flex items-center bg-gold-400 rounded-lg text-white py-4 px-6`}
     >
       {text}
       <span className="ml-3">
