63c574b77fa98c977e2100e73c85df0a37eff35f
Tomas Gonzalez <tomasg88@gmail.com>
2022-12-02 16:11:54 -0300

    setting up local fonts

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,4 +1,9 @@
 import { Crimson_Pro, Playfair_Display } from '@next/font/google';
+import localFont from '@next/font/local'
+
+export const billionReach = localFont({
+  src: '../public/fonts/Billion-Reach.otf'
+});
 
 export const crimson = Crimson_Pro({
   subsets: ['latin'],
@@ -9,3 +14,24 @@ export const playfair = Playfair_Display({
   subsets: ['latin'],
   weight: ['400'],
 });
+
+export const proximaNova = localFont({
+  src: [
+    {
+      path: '../public/fonts/Proxima-Nova-Black.otf',
+      weight: '900',
+    },
+    {
+      path: '../public/fonts/Proxima-Nova-Bold.otf',
+      weight: '600',
+    },
+    {
+      path: '../public/fonts/Proxima-Nova-Extrabold.otf',
+      weight: '800',
+    },
+    {
+      path: '../public/fonts/Proxima-Nova-Regular.otf',
+      weight: '400',
+    },
+  ]
+})
