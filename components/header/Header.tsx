df8dda036c1ca21131c27152733e7b5250337eb4
Tomas Gonzalez Soldavini <tomasg88@gmail.com>
2022-12-08 20:36:44 -0300

    Feat/layout (#11)
    * navigation font styling
    
    * add top navigation logo
    
    * add footer
    
    * remove ul and li
    
    * footer styling
    
    * footer icons
    
    * more foter
    
    * remove max-width in header and footer

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,7 +4,7 @@ import Navigation from '../navigation/Navigation';
 
 const Header = () => {
   return (
-    <header className="absolute flex justify-between items-center px-20 h-28 2xl:max-w-7xl w-full mx-auto right-0 left-0">
+    <header className="absolute flex justify-between items-center px-20 h-28  w-full mx-auto right-0 left-0">
       <div className="block h-20 w-56">
         <Image
           alt="Enciende Tu Poder"
