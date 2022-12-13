73f63a7f5f095f13c3f1c9ec1d8587f4e40fed15
Tomas Gonzalez <tomasg88@gmail.com>
2022-12-13 12:15:06 -0300

    remove home boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,7 @@
-import styles from '../styles/Home.module.css';
-
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className={styles.description}>
-        Get started by editing <code className={styles.code}>pages/index.tsx</code>
-      </p>
-
-      <div className={styles.grid}>
-        <a className={styles.card} href="https://nextjs.org/docs">
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a className={styles.card} href="https://nextjs.org/learn">
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a className={styles.card} href="https://github.com/vercel/next.js/tree/canary/examples">
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          className={styles.card}
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
+    <div className="min-h-screen bg-salmon-300">
+      <h1>HOME</h1>
     </div>
   );
 }
