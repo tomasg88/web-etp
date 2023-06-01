a3eb8a48f1755f9596914def4b593e1cae0a9e44
Tomás González <tomasg88@gmail.com>
2023-06-01 10:53:22 -0300

    background opacity, animation, dom nesting fix

--- modules/services/components/modal/SessionDIfferencesModal.tsx
+++ modules/services/components/modal/SessionDIfferencesModal.tsx
@@ -11,26 +11,28 @@ const SessionDifferencesModal = () => (
       </button>
     </Dialog.Trigger>
     <Dialog.Portal>
-      <Dialog.Overlay className="bg-black opacity-80 animate-overlay-show fixed inset-0 z-10" />
+      <Dialog.Overlay className="bg-black/80 backdrop-blur-sm data-[state=open]:animate-overlayShow fixed inset-0 z-10" />
       <Dialog.Content
-        className="animate-content-show fixed top-[50%] left-[50%] max-h-[85vh] w-full max-w-[1000px] translate-x-[-50%] translate-y-[-50%] rounded-[32px] bg-skin-200 py-12 px-20 focus:outline-none z-10"
+        className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-full max-w-[1000px] translate-x-[-50%] translate-y-[-50%] rounded-[32px] bg-white focus:outline-none z-10"
         style={{
           backgroundImage: 'url(./vector-13.png)',
           backgroundRepeat: 'no-repeat',
           backgroundPosition: 'center',
         }}
       >
-        <div>
+        <div className="text-center bg-skin-200/40 py-12 px-20 rounded-[32px]">
           <Dialog.Title className="text-purple-400 text-xs tracking-[0.14em] leading-3 font-bold text-center mb-16">
             DIFERENCIA
           </Dialog.Title>
           <Dialog.Description
-            className={`${playfairDisplay.variable} font-playfair-display text-center`}
+            className={`${playfairDisplay.variable} font-playfair-display`}
           >
+            <p
+              className={`${playfairDisplay.variable} font-playfair-display text-gold-600 text-[40px] leading-[43px] tracking-wide mb-9 line-clamp-2 px-20`}
+            >
+              ¿Cuál es la diferencia entre la sesión a Distancia y Presencial?
+            </p>
             <div>
-              <h1 className="text-gold-600 text-[40px] leading-[43px] tracking-wide mb-9 line-clamp-2 px-20">
-                ¿Cuál es la diferencia entre la sesión a Distancia y Presencial?
-              </h1>
               <p className="text-2xl text-gray-800 text-opacity-80 leading-8 px-[160px]">
                 La energía es la misma. A distancia la energía llega y se siente
                 perfectamente igual.
